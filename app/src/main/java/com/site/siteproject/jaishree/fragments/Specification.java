408c92b07b749dfb186e58f46e86f157ec527e9d
Prakash <akshav00@gmail.com>
2017-11-14 21:51:42 +0530

    specif dialog

--- app/src/main/java/com/site/siteproject/jaishree/fragments/Specification.java
+++ app/src/main/java/com/site/siteproject/jaishree/fragments/Specification.java
@@ -3,6 +3,7 @@ package com.site.siteproject.jaishree.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,6 +13,8 @@ import android.view.ViewGroup;
 import com.site.siteproject.R;
 import com.site.siteproject.jaishree.adapter.SpecificationAdapter;
 
+import java.util.ArrayList;
+
 /**
  * Created by Creative IT Works on 14-Nov-17.
  */
@@ -19,6 +22,9 @@ import com.site.siteproject.jaishree.adapter.SpecificationAdapter;
 public class Specification extends Fragment{
     RecyclerView specification_list;
     SpecificationAdapter adapter;
+
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,7 +33,7 @@ public class Specification extends Fragment{
         specification_list=(RecyclerView)view.findViewById(R.id.specification_list);
 
         adapter = new SpecificationAdapter(getActivity());
-        LinearLayoutManager ll = new LinearLayoutManager(getActivity());
+        GridLayoutManager ll = new GridLayoutManager(getActivity(),2);
         ll.setOrientation(LinearLayoutManager.VERTICAL);
         specification_list.setLayoutManager(ll);
 
